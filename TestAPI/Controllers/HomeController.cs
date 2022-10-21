6f9e6b54d7d1ee497021a71d54c56f064c4a45ac
Eng. Mohamad Omran Qanbour <mohamadqanbour@hotmail.com>
2022-10-22 00:34:22 +0400

    TestAPI

--- TestAPI/Controllers/HomeController.cs
+++ TestAPI/Controllers/HomeController.cs
@@ -43,10 +43,7 @@ namespace TestAPI.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
